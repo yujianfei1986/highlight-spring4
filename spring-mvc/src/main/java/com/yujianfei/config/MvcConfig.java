d01d5dffff192703475076d4ed3dbbc5008e0eb4
daredevil1986 <daredevil1986@126.com>
2020-04-18 22:23:37 +0800

    MVC Interceptor

--- spring-mvc/src/main/java/com/yujianfei/config/MvcConfig.java
+++ spring-mvc/src/main/java/com/yujianfei/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package com.yujianfei.config;
 
+import com.yujianfei.interceptor.MvcInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -13,6 +15,11 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan("com.yujianfei")
 public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public MvcInterceptor mvcInterceptor() {
+        return new MvcInterceptor();
+    }
 
     @Bean
     public InternalResourceViewResolver viewResolver() {
@@ -26,5 +33,10 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(mvcInterceptor());
     }
 }
---
+++ spring-mvc/src/main/java/com/yujianfei/interceptor/MvcInterceptor.java
@@ -0,0 +1,25 @@
+package com.yujianfei.interceptor;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MvcInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        long startTime = (long) request.getAttribute("startTime");
+        request.removeAttribute("startTime");
+        long endTime = System.currentTimeMillis();
+        System.out.println("本次请求的处理时间为:" + (endTime - startTime) + "ms");
+    }
+}
